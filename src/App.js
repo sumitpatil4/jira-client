7714c1f95917deb79875670cb904d1683b6239d4
YashAmbekar <yash.ambekar@accolitedigital.com>
2023-06-20 17:26:12 +0530

    merge new app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
-import Homepage from './components/Homepage/Homepage';
 
-const App = () => {
+import React from "react";
+import './App.css';
+import { Route, Switch } from "react-router-dom"
+import Backlog from './components/Backlog';
+import Roadmap from './components/Roadmap';
+import Board from './components/Board';
+import Setting from "./components/Setting"
+import LandingPage from './components/LandingPage/LandingPage';
+import Project from "./components/createproject/CreateProject";
+
+
+function App() {
+
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/board" component={Board} />
+        <Route exact path="/roadmap" component={Roadmap} />
+        <Route exact path="/backlog" component={Backlog} />
+        <Route exact path="/setting" component={Setting} />
+        <Route exact path="/project" component={Project} />
+      </Switch>
+
+    </>
   );
 };
 
