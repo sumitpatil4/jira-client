4baafd5ced700c59e4196e0a9ec893f52ba34019
YashAmbekar <yash.ambekar@accolitedigital.com>
2023-06-20 16:13:18 +0530

    landing page initial design without navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Homepage from './components/Homepage/Homepage';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      <LandingPage/>
     </div>
   );
 }
