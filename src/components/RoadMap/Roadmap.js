8f18ba1bb2a72d5377ba3099e689beb8e2ce2f8b
MridulSemwal <mridul.semwal@accolitedigital.com>
2023-06-24 11:38:48 +0530

    Added Roadmap file

---
+++ src/components/RoadMap/Roadmap.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// import Navbar from './common/Navbar'
+import Sidebar from '../common/sidebar/Sidebar'
+import { useState } from 'react';
+import "./Roadmap.css"
+
+export default function Backlog() {
+  const [sidebarOpen, setSideBarOpen] = useState(true);
+ const handleViewSidebar = () => {
+   setSideBarOpen(!sidebarOpen);
+ }
+
+  return (
+    <div className="setting-component">
+    <Sidebar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} />
+    <div className="setting-area">
+     <h1 className='heading'>Hello I am Roadmap Page</h1>
+   </div>
+   
+ </div>
+  )
+}
