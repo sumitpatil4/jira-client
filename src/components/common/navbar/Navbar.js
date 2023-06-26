7c595a658f5e55248586b6755002f2f663b7be2e
Charu Wadhwa1 <charu.wadhwa1@accolite.com>
2023-06-26 11:56:56 +0530

    Navbar Responsiveness

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -8,12 +8,17 @@ import Dropdown from "react-bootstrap/Dropdown";
 import Project from './Project'
 import YourWork from './YourWork'
 import Team from './teams/Team'
+import AuthContext from '../../ContextApi/AuthContext.js'
 export default function Navbar() {
-  //const authData = useContext(AuthContext);
-  //const userImageUrl = authData.googleData.picture;
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
+  const authData = useContext(AuthContext);
+  const userImageUrl = authData.userpicture;
   const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
   //const navigate = useNavigate();
   let navlinks = ["Your Work", "Projects", "Teams"]
   const unmount=()=>{
@@ -22,21 +27,19 @@ export default function Navbar() {
     
   }
   return (
-    <div className='outerWrapper'>
-      <div>
-        <Link to='/landingpage' style={{textDecoration:'none'}}>
-        <h1 style={{fontSize:"25px",fontWeight:"bold",color:"blue",marginTop:"1rem",marginLeft:"1.2rem"}}>Accolite Jira</h1>
-        </Link>
-      </div>
-      <div className='menu-icon' onClick={handleClick}>
-          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-      </div>
+    <nav className="navbar">
+    <div className="container">
+    <div>
+      <Link to='/landingpage' style={{textDecoration:'none'}}>
+       <h2 style={{fontSize:"25px",fontWeight:"bold",color:"blue",marginTop:"-1.5rem",marginLeft:"1rem",position:"fixed"}} className="dropdown">Accolite Jira</h2>
+       </Link>
+    </div>
       <div className='nav-links'>
         <ul className='nav-links'>
           {navlinks.map((curItem,index) => {
             return (
               <div class="dropdown">
-                <button onClick={closeMobileMenu} class="dropbtn">{curItem}&nbsp;<i className='fas fa-caret-down' /></button>
+                <button class="dropbtn">{curItem}&nbsp;<i className='fas fa-caret-down' /></button>
                 <div className='dropdown-content'>
                   {
                     index===0?(<YourWork/>)
@@ -49,9 +52,11 @@ export default function Navbar() {
             )
           })
           }
-        </ul>
+        <div>
         <button className='button-style'><b>Create</b></button>
-        <div className="profile">
+        </div>
+        <div>
+        <div className="profile drop-btn">
           <div className="NavbarDropDown">
             <Dropdown className="MainprofileDiv">
               <Dropdown.Toggle
@@ -72,8 +77,10 @@ export default function Navbar() {
             </Dropdown>
           </div>
         </div>
+        </div>
+        </ul>
       </div>
-
-    </div>
+      </div>
+    </nav>
   )
 }
