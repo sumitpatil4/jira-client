7c7cddf883b430930b9e71841857cec6ef2330a5
Aakriti Singh <aakriti.singh1@accolite.com>
2023-06-27 10:59:26 +0530

    styled the accordions in roadmap section

--- src/components/RoadMap/Roadmap.js
+++ src/components/RoadMap/Roadmap.js
@@ -10,20 +10,78 @@ import {
   Typography,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '95%',
+  // border: '1px solid red',
+  
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
 
 export default function Roadmap() {
   const [sidebarOpen, setSideBarOpen] = useState(true);
   const handleViewSidebar = () => {
     setSideBarOpen(!sidebarOpen);
   };
+  
   return (
     <div className="roadmap-component">
       {/* <Navbar onClick={handleViewSidebar} /> */}
       <Sidebar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} />
       <div className="roadmap-area">
-        <h1 className="heading">Hello I am Roadmap Page</h1>
-        <Accordion>
+        {/* <h1 className="heading"></h1> */}
+        <div className="roadmapHeader">
+        {/* <Typography className="roadmap-heading" variant="h4" gutterBottom>Roadmap</Typography> */}
+        <h2 className="roadmap-heading">Roadmap</h2>
+      <span>
+      <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          </span>
+          </div>
+        <Accordion className="accordion-0">
           <AccordionSummary
+            className="accordion-summary"
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1a-content"
             id="panel1a-header"
@@ -31,8 +89,9 @@ export default function Roadmap() {
             <Typography>PI 1</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Accordion>
+            <Accordion className="accordion-0">
               <AccordionSummary
+                className="accordion-summary"
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls="panel1a-content"
                 id="panel1a-header"
@@ -41,19 +100,18 @@ export default function Roadmap() {
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  <Accordion>
+                  <Accordion className="accordion-0">
                     <AccordionSummary
+                      className="accordion-summary"
                       expandIcon={<ExpandMoreIcon />}
                       aria-controls="panel1a-content"
                       id="panel1a-header"
                     >
                       <Typography>Issue 1</Typography>
                     </AccordionSummary>
-                    <AccordionDetails>
+                    <AccordionDetails className="accordion-details">
                       <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Suspendisse malesuada lacus ex, sit amet blandit leo
-                        lobortis eget.
+                        Fix the side panel in the roadmap and board page.
                       </Typography>
                     </AccordionDetails>
                   </Accordion>
@@ -62,8 +120,9 @@ export default function Roadmap() {
             </Accordion>
           </AccordionDetails>
         </Accordion>
-        <Accordion>
+        <Accordion  className="accordion-0">
           <AccordionSummary
+            className="accordion-summary"
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1a-content"
             id="panel1a-header"
@@ -71,8 +130,9 @@ export default function Roadmap() {
             <Typography>PI 2</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Accordion>
+            <Accordion className="accordion-0">
               <AccordionSummary
+                className="accordion-summary"
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls="panel1a-content"
                 id="panel1a-header"
@@ -81,19 +141,18 @@ export default function Roadmap() {
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  <Accordion>
+                  <Accordion className="accordion-0">
                     <AccordionSummary
+                      className="accordion-summary"
                       expandIcon={<ExpandMoreIcon />}
                       aria-controls="panel1a-content"
                       id="panel1a-header"
                     >
                       <Typography>Issue 1</Typography>
                     </AccordionSummary>
-                    <AccordionDetails>
+                    <AccordionDetails className="accordion-details">
                       <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Suspendisse malesuada lacus ex, sit amet blandit leo
-                        lobortis eget.
+                        Change the color of the Navbar.
                       </Typography>
                     </AccordionDetails>
                   </Accordion>
